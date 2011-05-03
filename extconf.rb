90889cd196003cdc90e99f1b65eded7640ed90c0
Matthew Stephens <mattbastard@gmail.com>
2011-05-02 23:34:07 -0400

    first working version

--- extconf.rb
+++ extconf.rb
@@ -7,6 +7,17 @@ require 'mkmf'
 # e.g. --with-sleuthkit-include=/path/to/tsk/include
 # --with-sleuthkit-lib=/path/to/tsk/lib
 # --with-sleuthkit-dir=/path/to/tsk is a shortcut for the previous two
-dir_config("sleuthkit")
-
+HEADER_DIRS=[
+"/Users/ms3uf/RubyTSK/sleuthkit-3.2.1",
+"/usr/local/include"
+]
+LIB_DIRS=[
+"/usr/local/lib"
+]
+dir_config("tsk3", HEADER_DIRS, LIB_DIRS)
+have_library("tsk3")
+unless find_header('tsk3/libtsk.h')
+  abort "libtsk.h is missing.  please install libtsk.h"
+end
 create_makefile("tsk4r")
+
