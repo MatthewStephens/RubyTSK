863e6f2db326961b2b1723197b35dc2ad8c1451b
Matthew Stephens <mattbastard@gmail.com>
2012-08-08 08:59:45 -0400

    whitespace police

--- ext/tsk4r/tsk4r.c
+++ ext/tsk4r/tsk4r.c
@@ -62,13 +62,13 @@ VALUE klass;
 
 // Sleuthkit::Image struct
 static struct tsk4r_wrapper {
-	TSK_IMG_INFO * image;
-	char * fn_given;
+  TSK_IMG_INFO * image;
+  char * fn_given;
 }tsk4r_wrapper;
 // Sleuthkit::Volume struct
 static struct tsk4r_vs_wrapper {
-	TSK_VS_INFO * volume;
-//	tsk4r_wrapper * disk;
+  TSK_VS_INFO * volume;
+//  tsk4r_wrapper * disk;
 }tsk4r_vs_wrapper;
 // Sleuthkit::FileSystem struct
 static struct tsk4r_fs_wrapper {
@@ -77,16 +77,16 @@ static struct tsk4r_fs_wrapper {
 
 // alloc & dealloc
 static void deallocate_image(struct tsk4r_wrapper * ptr){
-	//xfree((TSK_IMG_INFO *)image);
-	TSK_IMG_INFO *image = ptr->image;
-	tsk_img_close(image);
-	xfree(ptr);
+  //xfree((TSK_IMG_INFO *)image);
+  TSK_IMG_INFO *image = ptr->image;
+  tsk_img_close(image);
+  xfree(ptr);
 }
 
 static void deallocate_volume(struct tsk4r_vs_wrapper * ptr){
-	TSK_VS_INFO *volume = ptr->volume;
-	tsk_vs_close(volume);
-	xfree(ptr);
+  TSK_VS_INFO *volume = ptr->volume;
+  tsk_vs_close(volume);
+  xfree(ptr);
 }
 static void deallocate_filesystem(struct tsk4r_fs_wrapper * ptr){
     TSK_FS_INFO *filesystem = ptr->filesystem;
@@ -95,24 +95,24 @@ static void deallocate_filesystem(struct tsk4r_fs_wrapper * ptr){
 }
 
 static VALUE allocate_image(VALUE klass){
-//	TSK_IMG_INFO *image = ALLOC(TSK_IMG_INFO);
-//	TSK_IMG_INFO * image = malloc(sizeof(TSK_IMG_INFO));
-//	fprintf(stdout, "allocation complete.\n");
-//	return Data_Wrap_Struct(klass, 0, 0, image);
+//  TSK_IMG_INFO *image = ALLOC(TSK_IMG_INFO);
+//  TSK_IMG_INFO * image = malloc(sizeof(TSK_IMG_INFO));
+//  fprintf(stdout, "allocation complete.\n");
+//  return Data_Wrap_Struct(klass, 0, 0, image);
 
-	//struct tsk4r_wrapper * ptr = malloc(sizeof(struct tsk4r_wrapper)); // worked without malloc.  Do I need this?
-	struct tsk4r_wrapper * ptr = ALLOC(struct tsk4r_wrapper); // this might be improvement over previous
-	return Data_Make_Struct(klass, struct tsk4r_wrapper, 0, deallocate_image, ptr);
-	//return Data_Wrap_Struct(klass, 0, deallocate, ptr);
-	/*
-	TSK_IMG_INFO *sval;
-	return Data_Make_Struct(klass, TSK_IMG_INFO, 0, deallocate, sval);
-	 */
+  //struct tsk4r_wrapper * ptr = malloc(sizeof(struct tsk4r_wrapper)); // worked without malloc.  Do I need this?
+  struct tsk4r_wrapper * ptr = ALLOC(struct tsk4r_wrapper); // this might be improvement over previous
+  return Data_Make_Struct(klass, struct tsk4r_wrapper, 0, deallocate_image, ptr);
+  //return Data_Wrap_Struct(klass, 0, deallocate, ptr);
+  /*
+  TSK_IMG_INFO *sval;
+  return Data_Make_Struct(klass, TSK_IMG_INFO, 0, deallocate, sval);
+   */
 }
 static VALUE allocate_volume(VALUE klass){
-	struct tsk4r_vs_wrapper * ptr;
-	ptr = ALLOC(struct tsk4r_vs_wrapper);
-	return Data_Make_Struct(klass, struct tsk4r_vs_wrapper, 0, deallocate_volume, ptr);
+  struct tsk4r_vs_wrapper * ptr;
+  ptr = ALLOC(struct tsk4r_vs_wrapper);
+  return Data_Make_Struct(klass, struct tsk4r_vs_wrapper, 0, deallocate_volume, ptr);
 }
 
 static VALUE allocate_filesystem(VALUE klass){
@@ -124,59 +124,59 @@ static VALUE allocate_filesystem(VALUE klass){
 }
 
 static VALUE image_open(VALUE self, VALUE filename_str) {
-	char * filename;
-	struct tsk4r_wrapper * ptr;
-	Data_Get_Struct(self, struct tsk4r_wrapper, ptr);
+  char * filename;
+  struct tsk4r_wrapper * ptr;
+  Data_Get_Struct(self, struct tsk4r_wrapper, ptr);
 
-	VALUE img_size;
-	VALUE img_sector_size;
-	fprintf(stdout, "opening %s.\n", StringValuePtr(filename_str));
+  VALUE img_size;
+  VALUE img_sector_size;
+  fprintf(stdout, "opening %s.\n", StringValuePtr(filename_str));
 
-	Check_Type(filename_str, T_STRING);
-	rb_str_modify(filename_str);
-	filename=StringValuePtr(filename_str);
-	ptr->fn_given = (char *)filename;
-	ptr->image = tsk_img_open_sing(filename, TSK_IMG_TYPE_DETECT, 0);
-	fprintf(stdout, "attempt to open %s complete\n", (char *)filename);
-	if (ptr->image == NULL) {
-		rb_warn("unable to open disk.\n");
-	}
-	TSK_IMG_INFO *image = ptr->image;
-	
-	img_size = INT2NUM((int)image->size);
-	rb_iv_set(self, "@size", img_size);
-	img_sector_size = INT2NUM((int)image->sector_size);
-	rb_iv_set(self, "@sec_size", img_sector_size);
-	rb_iv_set(self, "@type", INT2NUM((int)image->itype));
-	
-	fprintf(stdout, "opening disk image of %d bytes.\n", (int)image->size ); // dev only
-	fprintf(stdout, "disk image has sectors %d bytes in size.\n", (int)image->sector_size ); // dev only
+  Check_Type(filename_str, T_STRING);
+  rb_str_modify(filename_str);
+  filename=StringValuePtr(filename_str);
+  ptr->fn_given = (char *)filename;
+  ptr->image = tsk_img_open_sing(filename, TSK_IMG_TYPE_DETECT, 0);
+  fprintf(stdout, "attempt to open %s complete\n", (char *)filename);
+  if (ptr->image == NULL) {
+    rb_warn("unable to open disk.\n");
+  }
+  TSK_IMG_INFO *image = ptr->image;
+  
+  img_size = INT2NUM((int)image->size);
+  rb_iv_set(self, "@size", img_size);
+  img_sector_size = INT2NUM((int)image->sector_size);
+  rb_iv_set(self, "@sec_size", img_sector_size);
+  rb_iv_set(self, "@type", INT2NUM((int)image->itype));
+  
+  fprintf(stdout, "opening disk image of %d bytes.\n", (int)image->size ); // dev only
+  fprintf(stdout, "disk image has sectors %d bytes in size.\n", (int)image->sector_size ); // dev only
 
-	return self;
+  return self;
 }
 
 // init an Image object (was lacking 'static' earlier)
 static VALUE initialize_disk_image(int argc, VALUE *args, VALUE self){
-	VALUE fn;
-//	static struct tsk4r_wrapper * ptr;
-	rb_scan_args(argc, args, "01", &fn);
-	if( ! NIL_P(fn)) {
-		image_open(self, fn);
-	} 
-	return self;
+  VALUE fn;
+//  static struct tsk4r_wrapper * ptr;
+  rb_scan_args(argc, args, "01", &fn);
+  if( ! NIL_P(fn)) {
+    image_open(self, fn);
+  } 
+  return self;
 }
 
 static VALUE initialize_volume(int argc, VALUE *args, VALUE self) {
-	VALUE offset; VALUE vs_type;
-	VALUE * img_obj;
-	rb_scan_args(argc, args, "11", &img_obj);
-	if (rb_obj_is_kind_of((VALUE)img_obj, rb_cClass1)) {
-		open_volume(self, (VALUE)img_obj);
-	} else {
-		rb_raise(rb_eTypeError, "Wrong argument type for arg1: (Sleuthkit::Image expected)");
-	}
-	
-	return self;
+  VALUE offset; VALUE vs_type;
+  VALUE * img_obj;
+  rb_scan_args(argc, args, "11", &img_obj);
+  if (rb_obj_is_kind_of((VALUE)img_obj, rb_cClass1)) {
+    open_volume(self, (VALUE)img_obj);
+  } else {
+    rb_raise(rb_eTypeError, "Wrong argument type for arg1: (Sleuthkit::Image expected)");
+  }
+  
+  return self;
 }
 
 static VALUE initialize_filesystem(int argc, VALUE *args, VALUE self){
@@ -207,31 +207,31 @@ static VALUE initialize_filesystem(int argc, VALUE *args, VALUE self){
 }
 
 static VALUE open_volume(VALUE self, VALUE img_obj) {
-	TSK_VS_INFO * vs_ptr;
-	struct tsk4r_wrapper * rb_image;
-//	TSK_VS_PART_INFO * partition_list;
-//	partition_list = vs_ptr->part_list;
-	Data_Get_Struct(img_obj, struct tsk4r_wrapper, rb_image);
-	TSK_IMG_INFO * disk = rb_image->image;
+  TSK_VS_INFO * vs_ptr;
+  struct tsk4r_wrapper * rb_image;
+//  TSK_VS_PART_INFO * partition_list;
+//  partition_list = vs_ptr->part_list;
+  Data_Get_Struct(img_obj, struct tsk4r_wrapper, rb_image);
+  TSK_IMG_INFO * disk = rb_image->image;
 
-	vs_ptr = tsk_vs_open(disk, 0, TSK_VS_TYPE_DETECT);
-	printf("disk has sector size: %d\n", (int)disk->sector_size );
-	printf("vs_ptr has partition count: %d\n", (int)vs_ptr->part_count);
-	printf("vs_ptr has vs_type: %d\n", (int)vs_ptr->vstype);
-	printf("vs_ptr type description: %s\n", (char *)tsk_vs_type_todesc(vs_ptr->vstype));
-	printf("vs_ptr has offset: %d\n", (int)vs_ptr->offset);
-	printf("vs_ptr has block size: %d\n", (int)vs_ptr->block_size);
-	printf("vs_ptr has endian: %d\n", (int)vs_ptr->endian);
-	
-	rb_iv_set(self, "@partition_count", INT2NUM((int)vs_ptr->part_count));
-	rb_iv_set(self, "@endian", INT2NUM((int)vs_ptr->endian));
-	rb_iv_set(self, "@offset", INT2NUM((int)vs_ptr->offset));
-	rb_iv_set(self, "@block_size", INT2NUM((int)vs_ptr->block_size));
-	
-//	printf("vs_part_ptr has desc: %s\n", (char *)partition_list->desc);
-//	printf("vs_part_ptr has start: %d\n", (int)partition_list->start);
-//	printf("vs_part_ptr has length: %d\n", (int)partition_list->len);
-//	fprintf(stdout, "I will open a volume, eventually.\n");
+  vs_ptr = tsk_vs_open(disk, 0, TSK_VS_TYPE_DETECT);
+  printf("disk has sector size: %d\n", (int)disk->sector_size );
+  printf("vs_ptr has partition count: %d\n", (int)vs_ptr->part_count);
+  printf("vs_ptr has vs_type: %d\n", (int)vs_ptr->vstype);
+  printf("vs_ptr type description: %s\n", (char *)tsk_vs_type_todesc(vs_ptr->vstype));
+  printf("vs_ptr has offset: %d\n", (int)vs_ptr->offset);
+  printf("vs_ptr has block size: %d\n", (int)vs_ptr->block_size);
+  printf("vs_ptr has endian: %d\n", (int)vs_ptr->endian);
+  
+  rb_iv_set(self, "@partition_count", INT2NUM((int)vs_ptr->part_count));
+  rb_iv_set(self, "@endian", INT2NUM((int)vs_ptr->endian));
+  rb_iv_set(self, "@offset", INT2NUM((int)vs_ptr->offset));
+  rb_iv_set(self, "@block_size", INT2NUM((int)vs_ptr->block_size));
+  
+//  printf("vs_part_ptr has desc: %s\n", (char *)partition_list->desc);
+//  printf("vs_part_ptr has start: %d\n", (int)partition_list->start);
+//  printf("vs_part_ptr has length: %d\n", (int)partition_list->len);
+//  fprintf(stdout, "I will open a volume, eventually.\n");
     return self;
 }
 
@@ -254,15 +254,15 @@ static VALUE open_filesystem(VALUE self, VALUE img_obj) {
         my_root_inum = fs_ptr->filesystem->root_inum; 
         my_last_inum = fs_ptr->filesystem->last_inum;
         my_block_size = fs_ptr->filesystem->block_size;
-				my_endian = fs_ptr->filesystem->endian;
-				my_offset = fs_ptr->filesystem->offset;
-				my_inum_count = fs_ptr->filesystem->inum_count;
-	      printf("fs_ptr has root_inum: %d\n", (int)fs_ptr->filesystem->root_inum);
-	      printf("fs_ptr has last_inum: %d\n", (int)fs_ptr->filesystem->last_inum);
-	      printf("fs_ptr has block_size: %d\n", (int)fs_ptr->filesystem->block_size);
-	      printf("fs_ptr has endian: %d\n", (int)fs_ptr->filesystem->endian);
-	      printf("fs_ptr has offset: %d\n", (int)fs_ptr->filesystem->offset);
-	      printf("fs_ptr has inum_count: %d\n", (int)fs_ptr->filesystem->inum_count);
+        my_endian = fs_ptr->filesystem->endian;
+        my_offset = fs_ptr->filesystem->offset;
+        my_inum_count = fs_ptr->filesystem->inum_count;
+        printf("fs_ptr has root_inum: %d\n", (int)fs_ptr->filesystem->root_inum);
+        printf("fs_ptr has last_inum: %d\n", (int)fs_ptr->filesystem->last_inum);
+        printf("fs_ptr has block_size: %d\n", (int)fs_ptr->filesystem->block_size);
+        printf("fs_ptr has endian: %d\n", (int)fs_ptr->filesystem->endian);
+        printf("fs_ptr has offset: %d\n", (int)fs_ptr->filesystem->offset);
+        printf("fs_ptr has inum_count: %d\n", (int)fs_ptr->filesystem->inum_count);
     } else {
         my_root_inum = 222;
         my_last_inum = 333;
@@ -298,107 +298,107 @@ static VALUE open_filesystem_from_vol(VALUE self, VALUE vol_obj) {
 }
 
 static VALUE close_volume(VALUE self){
-	return Qnil;
+  return Qnil;
 }
 
 static VALUE read_volume_block(int argc, VALUE *args, VALUE self){
-	return Qnil;
+  return Qnil;
 }
 
 static VALUE walk_volume(VALUE self){
-	return Qnil;
+  return Qnil;
 }
 
 static VALUE image_size(VALUE self){
-	TSK_IMG_INFO* image;
-	fprintf(stdout, "DATA_PTR(self): %lu\n", (long)DATA_PTR(self));
+  TSK_IMG_INFO* image;
+  fprintf(stdout, "DATA_PTR(self): %lu\n", (long)DATA_PTR(self));
 
-	Data_Get_Struct(self, TSK_IMG_INFO, image);
-	fprintf(stdout, "DATA_PTR(self): %lu\n", (long)DATA_PTR(self));
+  Data_Get_Struct(self, TSK_IMG_INFO, image);
+  fprintf(stdout, "DATA_PTR(self): %lu\n", (long)DATA_PTR(self));
 
-	//fprintf(stdout, "image size: %d\n", (int)image->size);
-	return INT2NUM((int)image->size);
+  //fprintf(stdout, "image size: %d\n", (int)image->size);
+  return INT2NUM((int)image->size);
 }
 
 static VALUE sector_size(VALUE self){
-	struct tsk4r_wrapper * new_ptr;	
-	Data_Get_Struct(self, struct tsk4r_wrapper, new_ptr);
+  struct tsk4r_wrapper * new_ptr;  
+  Data_Get_Struct(self, struct tsk4r_wrapper, new_ptr);
 
-	TSK_IMG_INFO * image = new_ptr->image;
-	char * orig_fn = new_ptr->fn_given;
-//	fprintf(stdout, "struct stored filename: >%s<\n", orig_fn);
-	VALUE s_size;
-	s_size = INT2FIX(22);
-	unsigned int sss = image->sector_size;
-	s_size = INT2FIX(sss);
-//	fprintf(stdout, "disk size: %d\n", (int)image->size);
-//	fprintf(stdout, "sector size: %d\n", sss);
-	FIXNUM_P(s_size);
-	return s_size;
+  TSK_IMG_INFO * image = new_ptr->image;
+  char * orig_fn = new_ptr->fn_given;
+//  fprintf(stdout, "struct stored filename: >%s<\n", orig_fn);
+  VALUE s_size;
+  s_size = INT2FIX(22);
+  unsigned int sss = image->sector_size;
+  s_size = INT2FIX(sss);
+//  fprintf(stdout, "disk size: %d\n", (int)image->size);
+//  fprintf(stdout, "sector size: %d\n", sss);
+  FIXNUM_P(s_size);
+  return s_size;
 }
 
 static VALUE image_type(VALUE self){
-	TSK_IMG_INFO *image;
-	Data_Get_Struct(self, TSK_IMG_INFO, image);
-	return INT2NUM((int)image->itype);
+  TSK_IMG_INFO *image;
+  Data_Get_Struct(self, TSK_IMG_INFO, image);
+  return INT2NUM((int)image->itype);
 }
 
 // The initialization method for this module
 void Init_tsk4r() {
-	rb_mtsk4r = rb_define_module("Sleuthkit");
-	
-	// class definitions
-	rb_cClass1 = rb_define_class_under(rb_mtsk4r, "Image", rb_cObject);
-	rb_cClass2 = rb_define_class_under(rb_mtsk4r, "Volume", rb_cObject);
-	rb_cClass3 = rb_define_class_under(rb_mtsk4r, "FileSystem", rb_cObject);
-	
-	// allocation functions
-	rb_define_alloc_func(rb_cClass1, allocate_image);
-	rb_define_alloc_func(rb_cClass2, allocate_volume);
+  rb_mtsk4r = rb_define_module("Sleuthkit");
+  
+  // class definitions
+  rb_cClass1 = rb_define_class_under(rb_mtsk4r, "Image", rb_cObject);
+  rb_cClass2 = rb_define_class_under(rb_mtsk4r, "Volume", rb_cObject);
+  rb_cClass3 = rb_define_class_under(rb_mtsk4r, "FileSystem", rb_cObject);
+  
+  // allocation functions
+  rb_define_alloc_func(rb_cClass1, allocate_image);
+  rb_define_alloc_func(rb_cClass2, allocate_volume);
   rb_define_alloc_func(rb_cClass3, allocate_filesystem);
 
 
-	// sub classes
-	//rb_cClass3 = rb_define_class_under(rb_cClass2, "ThirdClass", rb_cObject);
+  // sub classes
+  //rb_cClass3 = rb_define_class_under(rb_cClass2, "ThirdClass", rb_cObject);
 
-	
-	// some method templates
-	// a basic (module) methods -- note special function used to create
-	rb_define_module_function(rb_mtsk4r, "module_hello", method_hello_world, 0);
-	rb_define_module_function(rb_mtsk4r, "module_features", method_testMmethod, 0);
+  
+  // some method templates
+  // a basic (module) methods -- note special function used to create
+  rb_define_module_function(rb_mtsk4r, "module_hello", method_hello_world, 0);
+  rb_define_module_function(rb_mtsk4r, "module_features", method_testMmethod, 0);
 
-	// class methods
-	rb_define_module_function(rb_cClass1, "class_features", method_testCmethod1, 0);
-	rb_define_module_function(rb_cClass2, "class_features", method_testCmethod2, 0);
+  // class methods
+  rb_define_module_function(rb_cClass1, "class_features", method_testCmethod1, 0);
+  rb_define_module_function(rb_cClass2, "class_features", method_testCmethod2, 0);
 
-	// object methods for FirstClass objects
-	rb_define_method(rb_cClass1, "object_method_sample", method_testOmethod, 1); // change arg1 to klass?
-	rb_define_method(rb_cClass1, "object_hello", method_hello_world, 0); // change arg1 to klass?
-	rb_define_method(rb_cClass1, "initialize", initialize_disk_image, -1);
-	rb_define_method(rb_cClass1, "image_open", image_open, 1);
-	rb_define_method(rb_cClass1, "image_size", image_size, 0);
-	rb_define_method(rb_cClass1, "sector_size", sector_size, 0);
-	rb_define_method(rb_cClass1, "image_type", image_type, 0);
+  // object methods for FirstClass objects
+  rb_define_method(rb_cClass1, "object_method_sample", method_testOmethod, 1); // change arg1 to klass?
+  rb_define_method(rb_cClass1, "object_hello", method_hello_world, 0); // change arg1 to klass?
+  rb_define_method(rb_cClass1, "initialize", initialize_disk_image, -1);
+  rb_define_method(rb_cClass1, "image_open", image_open, 1);
+  rb_define_method(rb_cClass1, "image_size", image_size, 0);
+  rb_define_method(rb_cClass1, "sector_size", sector_size, 0);
+  rb_define_method(rb_cClass1, "image_type", image_type, 0);
 
-	// attributes (read only)
-	rb_define_attr(rb_cClass1, "size", 1, 0);
-	rb_define_attr(rb_cClass1, "sec_size", 1, 0);
-	rb_define_attr(rb_cClass1, "type", 1, 0);
+  // attributes (read only)
+  rb_define_attr(rb_cClass1, "size", 1, 0);
+  rb_define_attr(rb_cClass1, "sec_size", 1, 0);
+  rb_define_attr(rb_cClass1, "type", 1, 0);
 
-	
-	/* Sleuthkit::Volume */
-	// object methods for Volume objects
-	rb_define_method(rb_cClass2, "initialize", initialize_volume, -1);
-	rb_define_method(rb_cClass2, "open", open_volume, 1); // change arg1 to klass?
-	rb_define_method(rb_cClass2, "close", close_volume, 1);
-	rb_define_method(rb_cClass2, "read_block", read_volume_block, 3); //read block given start and no. of blocks
-	rb_define_method(rb_cClass2, "walk", walk_volume, 1);
-	
-	// attributes
-	rb_define_attr(rb_cClass2, "partition_count", 1, 0);
-	rb_define_attr(rb_cClass2, "endian", 1, 0);
-	rb_define_attr(rb_cClass2, "offset", 1, 0);
-	rb_define_attr(rb_cClass2, "block_size", 1, 0);
+  
+  /* Sleuthkit::Volume */
+  // object methods for Volume objects
+  rb_define_method(rb_cClass2, "initialize", initialize_volume, -1);
+  rb_define_method(rb_cClass2, "open", open_volume, 1); // change arg1 to klass?
+  rb_define_method(rb_cClass2, "close", close_volume, 1);
+  rb_define_method(rb_cClass2, "read_block", read_volume_block, 3); //read block given start and no. of blocks
+  rb_define_method(rb_cClass2, "walk", walk_volume, 1);
+  
+  // attributes
+  rb_define_attr(rb_cClass2, "partition_count", 1, 0);
+  rb_define_attr(rb_cClass2, "endian", 1, 0);
+  rb_define_attr(rb_cClass2, "offset", 1, 0);
+  rb_define_attr(rb_cClass2, "block_size", 1, 0);
     
     /* Sleuthkit::FileSystem */
     // object methods for FileSystem objects
@@ -419,27 +419,27 @@ void Init_tsk4r() {
 
 VALUE method_hello_world(VALUE klass)
 {
-	return rb_str_new2("hello world");
+  return rb_str_new2("hello world");
 }
 
 VALUE method_testMmethod(VALUE klass)
 {
-	return rb_str_new2("This is a module method responding.");
+  return rb_str_new2("This is a module method responding.");
 }
 
 VALUE method_testCmethod1(VALUE klass)
 {
-	return rb_str_new2("This is class method 1 responding.  I was assigned to Image");
+  return rb_str_new2("This is class method 1 responding.  I was assigned to Image");
 }
 
 VALUE method_testCmethod2(VALUE klass)
 {
-	return rb_str_new2("This is class method 2 responding.  I was assigned to Volume");
+  return rb_str_new2("This is class method 2 responding.  I was assigned to Volume");
 }
 
 VALUE method_testOmethod(VALUE klass)
 {
-	return rb_str_new2("I am simply an object method.");
+  return rb_str_new2("I am simply an object method.");
 }
 
 
