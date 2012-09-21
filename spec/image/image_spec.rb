652bae7949a5570fa232063544e7e62cf3ae4676
Matthew Stephens <mattbastard@gmail.com>
2012-09-21 15:15:11 -0400

    adding new tests

--- spec/image/image_spec.rb
+++ spec/image/image_spec.rb
@@ -1,20 +1,35 @@
 describe Sleuthkit::Image do
   before :all do
+    @orig_stderr = $stderr
+    $stderr = StringIO.new
+      
 		@sample_image = "spec/samples/tsk4r_img_01.dmg"
 		puts "File #{@sample_image} not found!!" unless File.exist?(@sample_image)
 	end
 
-	describe "#new" do
-		it "initializes without parameters" do
-			@image = Sleuthkit::Image.new
-			@image.should be_an_instance_of Sleuthkit::Image
-		end
-	end
 	describe "#new(@sample_image)" do
 		it "initializes with #{@sample_image} passed as arg1" do
 			@image = Sleuthkit::Image.new(@sample_image)
 			@image.should be_an_instance_of Sleuthkit::Image
 		end
 	end
+	describe "#new(@sample_image, 1)" do
+		it "initializes with specified TSK_IMG_TYPE_ENUM value of 1" do
+			@image = Sleuthkit::Image.new(@sample_image, 1)
+			@image.should be_an_instance_of Sleuthkit::Image
+		end
+	end
+	describe "#new(@sammple_image, 3)" do
+	  it "should fail to open disk when flag set to 3" do
+	    @image = Sleuthkit::Image.new(@sample_image, 3)
+	    @image.instance_variables.should eq []
+	    $stderr.rewind
+      $stderr.string.chomp.should match("unable to open disk")
+    end
+  end
+  
+  after :all do
+    $stderr = @orig_stderr
+  end
 end
 
